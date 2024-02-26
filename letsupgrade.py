99a97960264e03783ae6040ce0be06f8d8f68053
nikita1355 <nikitalandge36@gmail.com>
2024-02-26 20:47:05 +0530

    file added

---
+++ letsupgrade.py
@@ -0,0 +1,10 @@
+
+
+y=int(input("Enter a number : "))
+if y>0:
+    print("The Given Number Is Positive")
+elif y==0:
+    print("The Given Number Is Zero")
+elif y<0:
+    print("The Given Number Is Negative")
+
